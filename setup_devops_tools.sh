367645e836e58ff25b5e6f1ec02a703ccd78e0d3
James Flint <fewknow@Jamess-MacBook-Pro.local>
2022-04-26 15:14:59 -0400

    fix sourcing and messageing

--- setup_devops_tools.sh
+++ setup_devops_tools.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-#Need to reserve old bash_profile
-DATE=`date +%m%d%y%H%m%s`
-LOCATION="${HOME}/.bash_profile_${DATE}"
-echo "sudo mv ${HOME}/.bash_profile ${LOCATION}"
-sudo mv ${HOME}/.bash_profile ${LOCATION}
-
-
-#copy new bash profile
-echo "sudo cp ./devops_bash_profile ${HOME}/.bash_profile "
-sudo cp ./devops_bash_profile ${HOME}/.bash_profile
-
 #source
 echo "source ~/.bash_profile"
 source ${HOME}/.bash_profile
